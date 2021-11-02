59fe1b18bfe62e66cd50620ab4e1dfbf3656a523
dariodev <dario@@gmail.com>
2021-11-02 11:04:15 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import Template from './templates/Template.js';
-console.log('hola');
+//IMPORT CON ALIAS GRACIAS A WEBCONFIG -> resolve
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
+
 
 (async function App() {
   const main = null || document.getElementById('main');
